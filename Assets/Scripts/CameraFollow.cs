42db39bcdaa29a7cd8326ae056bb1501dd451575
Meghna6969 <pitcraj@gmail.com>
2026-01-09 00:00:33 -0500

    Final commit of the night; Stable commit with kitchen done and ai working

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -16,4 +16,20 @@ public class CameraFollow : MonoBehaviour
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("Collided with the wall");
+            other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("Exited collision with the wall");
+            other.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
 }
